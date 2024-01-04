bfb615ac262efdac0ab87677ffb0174871562729
eMode18 <ewagandi@gmail.com>
2024-01-04 02:19:57 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
 """
-This script retrieves and displays information about an employee's
-TODO list progress
-from a REST API based on the provided employee ID.
-
-Requirements:
-- Utilizes the requests module
-- Accepts an integer as a parameter (employee ID)
-- Displays the employee's TODO list progress in a specific format
+Retrieves task-related data for an employee using the JSON Placeholder API
 """
 
 import requests
 import sys
 
-
-def fetch_employee_todo_progress(employee_id):
-    base_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    t_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    try:
-        user_response = requests.get(base_url)
-        user_data = user_response.json()
-        employee_name = user_data['name']
-        todos_response = requests.get(t_url)
-        todos_data = todos_response.json()
-        total_tasks = len(todos_data)
-        completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-        print(f"Employee {employee_name} is done with tasks "
-              f"({completed_tasks}/{total_tasks}): ")
-        for todo in todos_data:
-            if todo['completed']:
-                print(f"\t{todo['title']}")
-    except requests.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-    employee_id = int(sys.argv[1])
-    fetch_employee_todo_progress(employee_id)
+
+    base_url = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1]
+    tasks_url = f"{base_url}/todos?userId={employee_id}"
+    user_url = f"{base_url}/users/{employee_id}"
+    try:
+        tasks_response = requests.get(tasks_url)
+        user_response = requests.get(user_url)
+        tasks_data = tasks_response.json()
+        user_data = user_response.json()
+        if (tasks_response.status_code != 200 or
+           user_response.status_code != 200):
+            print("Failed to fetch data. Please check the employee ID.")
+            sys.exit(1)
+        total_tasks = len(tasks_data)
+        completed_tasks = sum(1 for task in tasks_data if task['completed'])
+        employee_name = user_data.get("name")
+        print(f"Employee {employee_name} is done with tasks "
+              f"({completed_tasks}/{total_tasks}): ")
+        for task in tasks_data:
+            if task['completed']:
+                print(f"\t{task['title']}")
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        sys.exit(1)
