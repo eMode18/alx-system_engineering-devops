e9d50ab6e190324d3f81c225c7621085a48d06aa
eMode18 <ewagandi@gmail.com>
2024-04-12 01:39:03 -0700

    Web stack debugging using strance

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# corrects incorrect `phpp` extensions to `php` in the WordPress configuration file.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
